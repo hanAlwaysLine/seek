173166975b28876397019929f0e87ffad84c3fe6
cuijiarui <782447871@qq.com>
2017-08-28 14:01:06 +0800

    创建分支--

--- src/cn/action/WifeAction.java
+++ src/cn/action/WifeAction.java
@@ -23,6 +23,7 @@ public class WifeAction extends ActionSupport {
 		list = wifeMapperService.getWife();
 		System.out.println("action");
 		System.out.println(list.size());
+		System.out.println("action");
 		return "success";
 	}
 
